85acad5149b6500d5bd4d06dc62ee50ae5b2980d
777salviy@gmail.com <777salviy@gmail.com>
2023-05-22 21:27:52 +0300

    example010 use array, find array index Please enter the commit message for your changes. Lines starting

--- Lection2/example010/Program.cs
+++ Lection2/example010/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int[] array = { 1, 12, 31, 4, 51, 16, 72, 18, 72 };
+
+int n = array.Length;
+int find = 72;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
