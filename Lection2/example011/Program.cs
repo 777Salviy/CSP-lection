c644e7bee5fd6dc76f08bfcdf4a8aabed1b61609
777salviy@gmail.com <777salviy@gmail.com>
2023-05-22 22:27:22 +0300

    example011 методы заполнения массива, печати массива, нахождения индекса в массиве

--- Lection2/example011/Program.cs
+++ Lection2/example011/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection)
+
+{
+    int lenth = collection.Length;
+    int index = 0;
+    while (index < lenth)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position += 1;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index += 1;
+    }
+    return position;
+}
+
+int[] array = new int[12];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 9);
+Console.WriteLine(pos);
+
