a5e33e5729e13e9ed93364623cbffa9e8e6494d7
777salviy@gmail.com <777salviy@gmail.com>
2023-05-31 22:01:31 +0300

    add example012 work with text & for in For with array

--- Lection3/examples012_Methods/Program.cs
+++ Lection3/examples012_Methods/Program.cs
@@ -69,13 +69,83 @@
 // string res = Method4(12, "OJ");
 // Console.WriteLine(res);
 
-// 5. Цикл в Цикле
+// 5. Цикл в Цикле.
 
-for (int i = 2; i <= 10; i++)
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10;j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+// 6. Работа с текстом.
+// В тексте нужно все пробелы заменить черточками,
+// Маленькие буквы "к" замениить большими буквами "К",
+// а маленькие "с" большими "С".
+
+// string text = "Н.Зеландия запустила космическую политику страны, "
+//             + "обозначив растущие геополитические риски, связанные с космосом, "
+//             + "и необходимостью работать с партнерами-единомышленниками "
+//             + "для защиты национальной безопасности страны.";
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+//     int lenght = text.Length;
+//     for (int i = 0; i < lenght; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+//     return result;
+// }
+// string newText = Replace(text,' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText,'с', 'С');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к','К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'р', 'R');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'и', 'i');
+// Console.WriteLine(newText);
+
+int[] arr = { 1, 5, 6, 1, 8, 7, 3, 7, 2 };
+
+void PringArray(int[] array)
 {
-    for (int j = 2; j <= 10;j++)
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
     {
-        Console.WriteLine($"{i} * {j} = {i*j}");
+        Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
 }
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+
+
+}
+
+PringArray(arr);
+SelectionSort(arr);
+
+PringArray(arr);
