56be290ac1355cdcd691bd88b19f2c2e288c50ec
777salviy@gmail.com <777salviy@gmail.com>
2023-06-08 22:51:37 +0300

    example013 add method estimate Fibonacci using recursion

--- Lection4/example013_recursion/Program.cs
+++ Lection4/example013_recursion/Program.cs
@@ -108,14 +108,29 @@
 
 // 4. Рекурсия. Пример на решениии факториала!
 
-double Factorial(int n)
+// double Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+// 5. Вычисление чисел Фибоначи с помощью рекурсии
+// f(1) = 1 
+// f(2) = 1 
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
 {
-    if (n == 1) return 1;
-    else return n * Factorial(n - 1);
+    if (n == 1 || n == 2) return n = 1;
+else return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
 for (int i = 1; i < 40; i++)
 {
-    Console.WriteLine($"{i}! = {Factorial(i)}");
+    Console.WriteLine($"{i} = {Fibonacci(i)}");
 }
-
